152f79b045daa417d1964bdb1324c3947abce4aa
darmsDD <ivandinizdobbin2@gmail.com>
2018-12-31 18:05:40 -0200

    The bee and the cloud now moves

--- Timber.cpp
+++ Timber.cpp
@@ -67,6 +67,9 @@ int main()
     float cloud2Speed = 0.0f;
     float cloud3Speed = 0.0f;
 
+    // Variables to control time itself
+    Clock clock;
+
     while(window.isOpen())
     {
         /*
@@ -80,6 +83,130 @@ int main()
         /*
         Update the scene
         */
+
+        // Measure time
+        Time dt = clock.restart();
+
+        //Setup the bee
+        if(!beeActive)
+        {
+            //How fast is the bee
+            srand((int)time(0)*10);
+            beeSpeed = (rand() % 200)+ 200;
+            
+            //How high is the bee
+            srand((int)time(0)*10);
+            float height = (rand() % 400)+400;
+            spriteBee.setPosition(2000,height);
+            beeActive = true;
+        }   
+
+        else
+        //move the bee
+        {
+            spriteBee.setPosition(
+                spriteBee.getPosition().x - 
+                (beeSpeed * dt.asSeconds()),
+                spriteBee.getPosition().y);
+            
+            //Has the bee reached the right hand of the screen?
+            if(spriteBee.getPosition().x< -100)
+            {
+                //Set it up ready to be a whole new cloud next frame
+                beeActive = false;
+            }
+        }
+
+        //Manage the clouds
+        //Cloud 1
+        if(!cloud1Active)
+        {
+            //How fast is the cloud
+            srand((int)time(0)*10);
+            cloud1Speed = (rand()%200);
+
+            //How high is the cloud
+            srand((int)time(0)*10);
+            float height = (rand()%150);
+            spriteCloud1.setPosition(-200,height);
+            cloud1Active = true;
+        }
+
+         else
+        //move the cloud 1
+        {
+            spriteCloud1.setPosition(
+                spriteCloud1.getPosition().x + 
+                (cloud1Speed * dt.asSeconds()),
+                spriteCloud1.getPosition().y);
+            
+            //Has the bee reached the right hand of the screen?
+            if(spriteCloud1.getPosition().x > 1920)
+            {
+                //Set it up ready to be a whole new cloud next frame
+               cloud1Active = false;
+            }
+        }
+
+         //Cloud 2
+        if(!cloud2Active)
+        {
+            //How fast is the cloud
+            srand((int)time(0)*20);
+            cloud2Speed = (rand()%200);
+
+            //How high is the cloud
+            srand((int)time(0)*20);
+            float height = (rand()%300)-50;
+            spriteCloud2.setPosition(-200,height);
+            cloud2Active = true;
+        }
+
+         else
+        //move the cloud 2
+        {
+            spriteCloud2.setPosition(
+                spriteCloud2.getPosition().x + 
+                (cloud2Speed * dt.asSeconds()),
+                spriteCloud2.getPosition().y);
+            
+            //Has the bee reached the right hand of the screen?
+            if(spriteCloud2.getPosition().x > 1920)
+            {
+                //Set it up ready to be a whole new cloud next frame
+               cloud2Active = false;
+            }
+        } 
+
+         //Cloud 3
+        if(!cloud3Active)
+        {
+            //How fast is the cloud
+            srand((int)time(0)*30);
+            cloud3Speed = (rand()%200);
+
+            //How high is the cloud
+            srand((int)time(0)*10);
+            float height = (rand()%450)-100;
+            spriteCloud3.setPosition(-200,height);
+            cloud3Active = true;
+        }
+
+         else
+        //move the cloud 3
+        {
+            spriteCloud3.setPosition(
+                spriteCloud3.getPosition().x + 
+                (cloud1Speed * dt.asSeconds()),
+                spriteCloud3.getPosition().y);
+            
+            //Has the bee reached the right hand of the screen?
+            if(spriteCloud3.getPosition().x > 1920)
+            {
+                //Set it up ready to be a whole new cloud next frame
+               cloud3Active = false;
+            }
+        }
 
         /*
         Draw the scene
